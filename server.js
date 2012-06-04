84bb59c01573088e48c8cda3ee681ff9403c6709
Colin Van Dyke <colin@everfi.com>
2012-06-04 11:04:42 -0400

    A few changes
    Updates the dependencies. Configure the test env with another port,
    use port from the settings to start the server. Add redis as the session
    store

--- server.js
+++ server.js
@@ -3,10 +3,13 @@
  * Module dependencies.
  */
 
-require('coffee-script')
+require('coffee-script');
 
-var express = require('express')
-  , pg = require('pg');
+var express = require('express'),
+    mongodb = require('mongoose'),
+    RedisStore = require('connect-redis')(express);
+
+require('express-namespace');
 
 var app = module.exports = express.createServer();
 
@@ -15,8 +18,14 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('port', 3000);
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: "345678safghjksdaftyusadfgjkhasdf7y3huuhw4uyiohafsdkjlfsdalkh",
+    store: new RedisStore()
+  }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -29,10 +38,19 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+app.configure('test', function(){
+  app.set('port', 3001);
+});
+
+// Helpers
+require('./apps/helpers')(app);
+
 // Routes
-require('./apps/authentication/routes')(app);
+require('./apps/badges/routes')(app);
+// require('./apps/authentication/routes')(app);
+// require('./apps/admin/routes')(app);
 
 
-app.listen(3000, function(){
+app.listen(app.settings.port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
